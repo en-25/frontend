642514b81910c95e041402df2bed85f5c6c37b7e
en-25 <95131855+en-25@users.noreply.github.com>
2024-07-04 02:32:15 +0530

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -1,1 +1,11 @@
-
+document.addEventListener('DOMContentLoaded', (event) => {
+  const registerBtn = document.getElementById('register-btn');
+  registerBtn.addEventListener('click', () => {
+    window.location = "signin/signup.html"
+  });
+  
+  const loginBtn = document.getElementById('login-btn');
+  loginBtn.addEventListener('click', () => {
+      window.location = "signin/login.html";
+  });
+});
